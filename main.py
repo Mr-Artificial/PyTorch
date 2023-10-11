3abe99369e383fe4574aef2521efdbdaec9f036f
Maxim Jackson <maxim.jackson@live.com>
2023-10-10 21:59:52 -0700

    2nd Test commit for making sure file and code_env is properly pushing to GitHub

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+
+
 def file_name():
-    print("hello, what is your file name")
+    file_path = input("What is your file path: ")
+    print("Thank you, the path " + file_path + " was found properly")
+
 
 if __name__ == '__main__':
     file_name()
