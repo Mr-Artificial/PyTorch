90dd05f627c62d52d4b57b5b180b9cd59824c202
Maxim Jackson <maxim.jackson@live.com>
2023-10-10 22:07:41 -0700

    3rd commit

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def file_name():
     file_path = input("What is your file path: ")
     print("Thank you, the path " + file_path + " was found properly")
 
+    file_structure = {
+        "name": "path_name",
+        "number": "number_path",
+    }
+
+    print(file_structure)
+
+    file_structure.pop("name")
+
+    print(file_structure)
+
 
 if __name__ == '__main__':
     file_name()
