ffe5bafd686c21c3d7a012e9e68a328762a1f920
Maxim Jackson <maxim.jackson@live.com>
2023-10-11 22:03:16 -0700

    starting OOP python programming before committing to the PyTorch machine learning

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 
+import systemOperations
+
+
+def main_script():
+    os.times()
+    drives = os.listdir("C:\\Users\\MrArtificial\\PycharmProjects")
+    print(drives)
+    systemOperations.name = "computer"
+
 
 def file_name():
+
     file_path = input("What is your file path: ")
     print("Thank you, the path " + file_path + " was found properly")
 
@@ -10,6 +20,11 @@ def file_name():
         "number": "number_path",
     }
 
+    folder_files = ["abd", "abc", "abe"]
+    folder_files.insert(0, "aba")
+    path_attributes = file_structure.pop("name")
+
 
 if __name__ == '__main__':
     file_name()
+    main_script()
---
+++ systemOperations.py
@@ -0,0 +1,8 @@
+
+class sysOps():
+    def __init__(self, name, system, operations):
+        self.name = name
+        self.system = system
+        self.operations = operations
+
+
