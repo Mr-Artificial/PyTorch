303dfaa1a7231c2ee64fafaa13f0ecb2acdc2a87
Maxim Jackson <maxim.jackson@live.com>
2023-11-01 16:54:17 -0700

    adding full user functionality, working on OOP protocols

--- banking.py
+++ banking.py
@@ -1,14 +1,21 @@
 class Banking:
-    def __init__(self, password, balance, withdraw, deposit, statement):
+    def __init__(self, username, password, balance, withdraw, deposit, statement):
+        self.username = username
         self.password = password
         self.balance = int(balance)
         self.withdraw = int(withdraw)
         self.deposit = int(deposit)
         self.statement = statement
 
+    def account_info(self, username, balance, statement):
+        return username + "'s account info: \n" +"\tBalance: " + str(balance) + "\n\tStatement: " + str(statement)
     def check_balance(self, balance):
         return balance
 
     def withdraw_funds(self, withdraw, balance):
         new_balance = balance - withdraw
         return "Your new account value is: " + str(new_balance) + "$, you withdrew: " + str(withdraw) + "$\n"
+
+    def deposit_fund(self, balance, deposit):
+        new_balance = balance + deposit
+        return "Your new account value is: " + str(new_balance) + "$, you deposited: " + str(deposit) + "$\n"
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from banking import *
 
 if __name__ == '__main__':
-    oAccount = Banking("abcd", 100, 20, 20, ["14$", "10$", "200$"])
+    oAccount = Banking("MaxJack", "abcd", 100, 20, 20, ["14$", "10$", "200$"])
     balance = oAccount.check_balance(100)
     print(str(balance) + '$')
     print(oAccount.withdraw_funds(10, balance))
+    print(oAccount.account_info("Maxjack", 1000, 10))
 
